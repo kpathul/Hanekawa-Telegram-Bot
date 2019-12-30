7d3ee24485cdb0721b1c90730bf54fe640ed407e
justinepdevasia <justinepdevasia@gmail.com>
2019-12-30 18:24:20 +0530

    xkcd meme extractor

---
+++ src/helper_functions/xkcd_meme.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+
+#generate valid random number for comic id 
+comicId = 200
+
+class Meme(object):
+    def __init__(self,comicId):
+        self.comicId = comicId
+        self.url = " http://xkcd.com/{0}/info.0.json".format(self.comicId)
+    
+    def getImageUrl(self):   
+        try:
+            self.query = requests.get(self.url)
+            self.jsonData = self.query.json()
+            return self.jsonData["img"]
+
+        except:
+            print("unable to get data")
+
+    
+    def getMemeData(self):
+        try:
+            self.query = requests.get(self.url)
+            self.jsonData = self.query.json()
+            return self.jsonData
+        except:
+            print("unable to get data")
